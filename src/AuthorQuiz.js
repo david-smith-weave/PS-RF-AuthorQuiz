b348fea1f3951fd9b74cd2d380271345d771d09f
David Smith <david.smith@weaveability.com>
2019-12-04 11:15:09 +0000

    replace default App content with Author Quiz title

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -5,21 +5,8 @@ import './bootstrap.min.css';
 
 function AuthorQuiz() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Author Quiz
     </div>
   );
 }
