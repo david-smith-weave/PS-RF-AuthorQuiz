a66ddbc109af76f8a7272314e2ca838400f5f64b
David Smith <david.smith@weaveability.com>
2019-12-05 11:18:12 +0000

    create new render function encapsulating ReactDOM

--- src/index.js
+++ src/index.js
@@ -76,8 +76,10 @@ function onAnswerSelected(answer){
     state.highlight = isCorrect ? 'correct' : 'wrong';
 }
 
-ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
-
+function render(){
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
