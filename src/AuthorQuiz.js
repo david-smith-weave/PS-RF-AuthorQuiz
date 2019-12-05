ef6c418b2da61226ecf90c8257b258accd5fe262
David Smith <david.smith@weaveability.com>
2019-12-05 08:43:27 +0000

    add book options

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -20,6 +20,9 @@ function Turn(){
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorimage" alt="Author"/>
       </div>
+      <div className="col-6">
+        {books.map((title) => <p>{title}</p>)}
+      </div>
     </div>
   );
 }
