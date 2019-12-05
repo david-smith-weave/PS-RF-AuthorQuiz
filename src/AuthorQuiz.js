faeeba313a3d8d84ea224dd9f85d6f1d6a37c409
David Smith <david.smith@weaveability.com>
2019-12-05 08:24:07 +0000

    added Hero, Turn, Continue component skeletons

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,10 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
+function Hero(){
+  return(
+    <div></div>
+  );
+}
+
+function Turn(){
+  return(
+    <div></div>
+  );
+}
+
+function Continue(){
+  return(
+    <div></div>
+  );
+}
+
 function AuthorQuiz() {
   return (
     <div className="container-fluid">
-      
+      <Hero />
+      <Turn />
+      <Continue />
     </div>
   );
 }
