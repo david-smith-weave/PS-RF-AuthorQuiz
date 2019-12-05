d2295bfc99ecdecc48d48f67a335496b82f65883
David Smith <david.smith@weaveability.com>
2019-12-05 08:33:50 +0000

    added hero banner title and content

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -7,7 +7,8 @@ function Hero(){
   return(
     <div className="row">
       <div className="jumbotron col-10 offset-1">
-        
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
       </div>
     </div>
   );
