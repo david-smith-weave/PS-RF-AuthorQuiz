511772aa5a1c0cbb4440e9ac0b52e4f5885843be
David Smith <david.smith@weaveability.com>
2019-12-05 08:53:43 +0000

    pass state to AuthorQuiz using spread

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const state = {
     }
 }
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
