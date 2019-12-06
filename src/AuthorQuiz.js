d12ea049de448c9f60a86685267aed115fe5594a
David Smith <david.smith@weaveability.com>
2019-12-06 09:52:51 +0000

    add switch

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
