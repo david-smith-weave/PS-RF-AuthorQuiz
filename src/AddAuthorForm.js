495e175f2a63c6d20fc6b8ab08a26ea12049574a
David Smith <david.smith@weaveability.com>
2019-12-06 11:22:45 +0000

    add state to form

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -2,15 +2,22 @@ import React from 'react';
 import "./AddAuthorForm.css";
 
 class AuthorForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            name: '',
+            imageUrl: ''
+        };
+    }
     render() {
         return <form>
             <div className="AddAuthorForm__input">
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" value={this.state.name}/>
             </div>
             <div className="AddAuthorForm__input">
                 <label htmlFor="imageUrl">Image URL</label>
-                <input type="text" name="imageURL" />
+                <input type="text" name="imageURL" value={this.state.imageUrl}/>
             </div>
         </form>
     }
