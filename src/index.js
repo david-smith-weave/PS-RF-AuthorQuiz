f3b7dbc62006ba3a0acc190d59da3811c4a30820
David Smith <david.smith@weaveability.com>
2019-12-10 10:21:36 +0000

    redux now handling render - function not required

--- src/index.js
+++ src/index.js
@@ -113,17 +113,15 @@ const AuthorWrapper = withRouter(({history}) =>
     }} />
 );
 
-function render(){
-    ReactDOM.render(
-        <BrowserRouter>
-            <React.Fragment>
-                <Route exact path="/" component={App} />
-                <Route path="/add" component={AuthorWrapper} />
-            </React.Fragment>
-        </BrowserRouter>, document.getElementById('root')
-    );
-}
-render();
+ReactDOM.render(
+    <BrowserRouter>
+        <React.Fragment>
+            <Route exact path="/" component={App} />
+            <Route path="/add" component={AuthorWrapper} />
+        </React.Fragment>
+    </BrowserRouter>, document.getElementById('root')
+);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
