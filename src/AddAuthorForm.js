5db870984630dfeb9743cea6a7f6a4fd6f354f46
David Smith <david.smith@weaveability.com>
2019-12-10 10:30:09 +0000

    add routing import for author form

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import "./AddAuthorForm.css";
 
 class AuthorForm extends React.Component {
